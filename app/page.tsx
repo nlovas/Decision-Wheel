e664f6e80d0a19afed75b8a6c47afedb861d0355
nlovas <lovasnicole@gmail.com>
2024-07-03 19:11:29 -0600

    added react konva library. adjusted to make it draw, needs some adjusting sizewise

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
-'use client';
+"use client";
 
-import Image from "next/image";
+/*import Image from "next/image";
+import { Wheel } from "../components/wheel";
+;
+*/
 import { Sidebar } from "./ui/sidebar";
-import { Wheel } from "./ui/wheel";
 import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import dynamic from 'next/dynamic';
 import { Task } from "./types";
+import { v4 as uuidv4 } from 'uuid'
+
+
+// react konva get errors if its rendered server side, so must use dynamic importing
+// see https://github.com/konvajs/react-konva/issues/787#issuecomment-2022484805
+const Wheel = dynamic(() => import("../components/wheel"), {
+	ssr: false,
+});
 
 export default function Home() {
+
   const [taskList, setTaskList] = useState(Array<Task>());
 
   function addTask(task: string){
@@ -39,19 +50,18 @@ export default function Home() {
     setTaskList([...newArray]);
   }
 
-
   return (
-    <main className="flex h-screen flex-col p-20">
-      <h1 className="w-full text-center">Decision Wheel</h1>
-      <div className="flex flex-row items-center justify-center mt-6 h-full border border-pink-400">
-        <div className="flex flex-col h-full relative basis-3/5 border border-yellow-300">
-          <Wheel taskList={taskList}/>
-          <div className="relative basis-2/12 border border-cyan-400">
-            <button className="absolute bottom-0 right-0" /*onClick={spinWheel}*/>Choose</button>
-          </div>
-        </div>
-        <Sidebar addTask={addTask} editTask={editTask} deleteTask={deleteTask} taskList={taskList}/>
+  <main className="flex h-screen flex-col p-20">
+  <h1 className="w-full text-center">Decision Wheel</h1>
+  <div className="flex flex-row items-center justify-center mt-6 h-full border border-pink-400">
+    <div className="flex flex-col h-full relative basis-3/5 border border-yellow-300">
+        <Wheel taskList={taskList}/>
+      <div className="relative basis-2/12 border border-cyan-400">
+        <button className="absolute bottom-0 right-0">Choose</button>
       </div>
-    </main>
+    </div>
+    <Sidebar addTask={addTask} editTask={editTask} deleteTask={deleteTask} taskList={taskList}/>
+  </div>
+  </main>
   );
 }
--- components/wheel.tsx
+++ components/wheel.tsx
@@ -1,27 +1,28 @@
 import { useEffect, useRef, useState } from "react";
-import { Task, Segment } from "../types";
-import Konva from "konva";
+import { Task, Segment } from "../app/types";
+import {Stage, Layer, Text, Circle} from "react-konva";
 
-export function Wheel({
-    taskList
-    }:{
-        taskList: Array<Task>
-    }){
+type Props = {
+	taskList?: Array<Task>;
+};
 
+function Wheel({ taskList }: Props) {
 
     // keeps track of the amount of circumference that each task is using
     const [segments, setSegments] = useState(Array<Segment>());
-    const [ctx, setCanvasContext] = useState<CanvasRenderingContext2D | null>(null);
+    //const [ctx, setCanvasContext] = useState<CanvasRenderingContext2D | null>(null);
     // useRef is for a mutable variable that doesnt trigger a change in the component
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    //const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
 
         console.log("useffect called");
-        const canvas = canvasRef.current as HTMLCanvasElement;//document.getElementById("canvas") as HTMLCanvasElement;
-        const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-        setCanvasContext(context);
+        //const canvas = canvasRef.current as HTMLCanvasElement;//document.getElementById("canvas") as HTMLCanvasElement;
+        //const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+        //setCanvasContext(context);
         //ctx?.save();
+
+
 
         window.addEventListener('resize', resizeCanvas, false);
         resizeCanvas();
@@ -30,22 +31,28 @@ export function Wheel({
     // and https://stackoverflow.com/a/10215724
     // THE MOST HELPFUL https://stackoverflow.com/a/33558386
     function resizeCanvas(){
-        const parent = document.getElementById("canvas-wrapper") as HTMLElement;
+        /*const parent = document.getElementById("canvas-wrapper") as HTMLElement;
         canvas.style.display = "none";
         const ow = parent.offsetWidth;
         const oh = parent.offsetHeight;
         canvas.height = oh;
         canvas.width = ow;
-        canvas.style.display = "block";
+        canvas.style.display = "block";*/
         //drawWheel(ow/2,oh/2);
         //drawPeg(ow/2, oh/2);
     }
 
     }, [taskList]);
 
-    return (
+	return (
         <div id="canvas-wrapper" className="basis-10/12 w-full relative border border-red-600">
-            <canvas ref={canvasRef} className="border border-green-950" id="canvas" width="1" height="1"></canvas>
+		<Stage width={window.innerWidth-100} height={window.innerHeight-100}>
+			<Layer>
+				<Circle x={500} y={300} radius={50} fill="green" />
+			</Layer>
+		</Stage>
         </div>
-    );
+	);
 }
+
+export default Wheel;
