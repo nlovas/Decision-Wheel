b4423a81305578b01bd7173df344955d97e46c7e
nlovas <lovasnicole@gmail.com>
2024-07-06 23:53:51 -0600

    Updated resizing function to work with konva, now need to get back to drawing the wheel in konva

--- components/wheel.tsx
+++ components/wheel.tsx
@@ -1,6 +1,10 @@
-import { useEffect, useRef, useState } from "react";
+import { ComponentProps, RefObject, useEffect, useRef, useState } from "react";
 import { Task, Segment } from "../app/types";
-import {Stage, Layer, Text, Circle} from "react-konva";
+import {Stage, Layer, Text, Circle, Star, KonvaNodeComponent, Group} from "react-konva";
+import { Animation } from "konva/lib/Animation";
+import { LayerConfig } from "konva/lib/Layer";
+import React from "react";
+import Konva from "konva";
 
 type Props = {
 	taskList?: Array<Task>;
@@ -10,17 +14,16 @@ function Wheel({ taskList }: Props) {
 
     // keeps track of the amount of circumference that each task is using
     const [segments, setSegments] = useState(Array<Segment>());
-    //const [ctx, setCanvasContext] = useState<CanvasRenderingContext2D | null>(null);
     // useRef is for a mutable variable that doesnt trigger a change in the component
-    //const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    const stageRef = React.useRef<Konva.Stage>(null);
+    const wheelLayerRef = React.useRef<Konva.Layer>(null);
+    const wheelGroupRef = React.useRef<Konva.Group>(null);
+    const canvasWrapperRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
 
         console.log("useffect called");
-        //const canvas = canvasRef.current as HTMLCanvasElement;//document.getElementById("canvas") as HTMLCanvasElement;
-        //const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-        //setCanvasContext(context);
-        //ctx?.save();
 
 
 
@@ -31,6 +34,16 @@ function Wheel({ taskList }: Props) {
     // and https://stackoverflow.com/a/10215724
     // THE MOST HELPFUL https://stackoverflow.com/a/33558386
     function resizeCanvas(){
+        if(stageRef.current && canvasWrapperRef.current) {
+            stageRef.current.visible(false);
+            const ow = canvasWrapperRef.current.offsetWidth;
+            const oh = canvasWrapperRef.current.offsetHeight;
+            stageRef.current.width(ow);
+            stageRef.current.height(oh);
+            console.log("wrapper", ow, oh);
+            console.log("stage", stageRef.current.width(), stageRef.current.height());
+            stageRef.current.visible(true);
+        }
         /*const parent = document.getElementById("canvas-wrapper") as HTMLElement;
         canvas.style.display = "none";
         const ow = parent.offsetWidth;
@@ -40,16 +53,30 @@ function Wheel({ taskList }: Props) {
         canvas.style.display = "block";*/
         //drawWheel(ow/2,oh/2);
         //drawPeg(ow/2, oh/2);
+        /*var anim = new Animation(frame => {
+            if(frame){
+            var angleDiff = (frame.timeDiff * 90) / 1000;
+            const wheelGroup = wheelGroupRef.current;
+            if(wheelGroup){
+                wheelGroup.rotate(angleDiff);
+            }
+            }
+
+        }, wheelLayerRef);
+        anim.start();*/
     }
 
     }, [taskList]);
 
 	return (
-        <div id="canvas-wrapper" className="basis-10/12 w-full relative border border-red-600">
-		<Stage width={window.innerWidth-100} height={window.innerHeight-100}>
-			<Layer>
-				<Circle x={500} y={300} radius={50} fill="green" />
-			</Layer>
+        <div ref={canvasWrapperRef} id="canvas-wrapper" className="basis-10/12 w-full relative border border-red-600">
+		<Stage ref={stageRef} width={1} height={1}>
+			<Layer ref={wheelLayerRef}>
+                <Group ref={wheelGroupRef}>
+				    <Circle x={500} y={300} radius={50} fill="green" />
+                    <Star x={100} y={100} numPoints={5} innerRadius={10} outerRadius={30} fill="blue"/>
+                </Group>
+            </Layer>
 		</Stage>
         </div>
 	);
