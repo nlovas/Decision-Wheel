8490d3db0357a0133f55f312855fb986cc1a77d7
nlovas <lovasnicole@gmail.com>
2024-08-16 19:14:00 -0600

    Added another test for the sidebar

--- __tests__/sidebar.test.tsx
+++ __tests__/sidebar.test.tsx
@@ -1,15 +1,7 @@
-//import renderer from 'react-test-renderer';
 import { Task } from "@/app/types";
 import { Sidebar } from "../app/ui/sidebar";
 import { render, screen } from "@testing-library/react";
-
-/*
-it('renders correctly', () => {
-    const tree = renderer
-        .create(<Sidebar></Sidebar)
-        .toJSON();
-        expect(tree).toMatchSnapshot();
-});*/
+import user from "@testing-library/user-event";
 
 test("renders a blank sidebar", () =>{
     const mockAddTask = jest.fn();
@@ -20,4 +12,21 @@ test("renders a blank sidebar", () =>{
     render(<Sidebar addTask={mockAddTask} editTask={mockEditTask} deleteTask={mockDeleteTask} taskList={mockTaskList}/>)
     const input = screen.getByLabelText("New Task");
     expect(input).toBeInstanceOf(HTMLInputElement);
+});
+
+test("creates a new task", async () =>{
+    const mockAddTask = jest.fn();
+    const mockEditTask = jest.fn();
+    const mockDeleteTask = jest.fn();
+    const mockTaskList = new Array<Task>();
+
+    render(<Sidebar addTask={mockAddTask} editTask={mockEditTask} deleteTask={mockDeleteTask} taskList={mockTaskList}/>)
+    const input = screen.getByLabelText("New Task");
+
+    await user.click(input);
+    await user.keyboard('laundry');
+
+    const submitBtn = screen.getByText('Ok');
+    await user.click(submitBtn);
+    expect(mockAddTask).toHaveBeenCalled();
 });
